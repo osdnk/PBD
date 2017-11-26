6c09ca16d7220345a5666a093f2a35b2371539b1
osdnk <micosa97@gmail.com>
2017-11-26 15:05:55 +0100

    Add some fake conferences data

--- pbd/lib/population.exs
+++ pbd/lib/population.exs
@@ -1,3 +1,13 @@
+import DataGenerator
 {:ok, pid} = Postgrex.start_link(hostname: "localhost", username: "postgres", password: "postgres", database: "pbd")
 data = Postgrex.query!(pid, "SELECT * FROM conferences", [])
-IO.inspect data
+
+
+conferences_dates = generate_conferences()
+conferenes_descriptions = []
+
+
+
+IO.inspect conferences_dates
+
+
--- pbd/mix.exs
+++ pbd/mix.exs
@@ -14,14 +14,15 @@ defmodule PBD.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :faker]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-        {:postgrex, "~> 0.13.3"}
+        {:postgrex, "~> 0.13.3"},
+        {:faker, "~> 0.9"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
