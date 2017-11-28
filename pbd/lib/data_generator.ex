3fd92abde44a05c0b2664a4f3208f48951ac21a5
Malenczuk <malen11pl@gmail.com>
2017-11-28 15:28:47 +0100

    Add some generate func reffering to clients

--- pbd/lib/data_generator.ex
+++ pbd/lib/data_generator.ex
@@ -1,7 +1,7 @@
 import List
 defmodule DataGenerator do
   @moduledoc false
-  def generate_dates do
+  def generate_conferences do
     first_date = ~D[2000-01-01]
     r = :rand.uniform(30)
     new_date = Date.add(first_date, r)
@@ -38,4 +38,46 @@ defmodule DataGenerator do
     }
   end
 
+  def generate_private_clients do
+    get_private_clients(add_pcdescription(), 10)
+  end
+
+  def get_private_clients(l, n) when n < 0 do
+    l
+  end
+
+  def get_private_clients(l, n) do
+    inc_list = [add_private_client_description() | l]
+    get_private_clients(inc_list, n-1)
+  end
+
+  def add_private_client_description do
+    %{FirstName: Faker.Name.first_name(),
+      LastName: Faker.Name.last_name(),
+      EMail: Faker.Internet.free_email(),
+      Phone: Faker.Phone.EnUs.phone(),
+      Address: Faker.Address.street_address()
+    }
+  end
+
+  def generate_company_clients do
+    get_company_clients(add_ccdescription(), 10)
+  end
+
+  def get_company_clients(l, n) when n < 0 do
+    l
+  end
+
+  def get_company_clients(l, n) do
+    inc_list = [add_ccdescription() | l]
+    get_cclients(inc_list, n-1)
+  end
+
+  def add_company_client_description do
+    %{Name: Faker.Company.name(),
+      EMail: Faker.Internet.free_email(),
+      Phone: Faker.Phone.EnUs.phone(),
+      Address: Faker.Address.street_address()
+    }
+  end
 end
--- pbd/lib/population.exs
+++ pbd/lib/population.exs
@@ -6,8 +6,15 @@ data = Postgrex.query!(pid, "SELECT * FROM conferences", [])
 conferences_dates = generate_conferences()
 conferenes_descriptions = []
 
+private_clients = generate_privateclients()
+
+company_clients = generate_companyclients()
+
 
 
 IO.inspect conferences_dates
 
+IO.inspect private_clients
+
+IO.inspect company_clients
 
